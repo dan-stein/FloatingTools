36ae312c39f14a2187e78cabaffdc91ae74caab2
alexhatfield <alex.e.hatfield@gmail.com>
2017-04-15 21:36:33 -0400

    Initial push for auto installer and update function.

--- FloatingTools/__init__.py
+++ FloatingTools/__init__.py
@@ -2,7 +2,9 @@
 import os
 
 # Global variables
-PACKAGES = os.path.join(os.path.dirname(__file__), 'packages')
+FLOATING_TOOLS_ROOT = os.path.dirname(__file__)
+INSTALL_DIRECTORY = os.path.dirname(FLOATING_TOOLS_ROOT)
+PACKAGES = os.path.join(FLOATING_TOOLS_ROOT, 'packages')
 
 # validate the install
 import install
--- FloatingTools/install.py
+++ FloatingTools/install.py
@@ -4,8 +4,12 @@ Validate the dependencies are installed.
 # python imports
 import os
 import sys
+import json
 import urllib
+import base64
+import datetime
 import subprocess
+from time import strptime
 
 # FloatingTools imports
 import FloatingTools
@@ -54,3 +58,108 @@ except ImportError:
 
     # verify install
     import flask
+
+
+def githubDate2Datetime(githubDate):
+    """
+    --private--
+    :param githubDate: 
+    :return: 
+    """
+    splitGHTime = githubDate.split(' ')
+    splitGHSmallTime = splitGHTime[4].split(':')
+    converted = datetime.datetime(
+        day=int(splitGHTime[1]),
+        month=strptime(splitGHTime[2], '%b').tm_mon,
+        year=int(splitGHTime[3]),
+        hour=int(splitGHSmallTime[0]),
+        minute=int(splitGHSmallTime[1]),
+        second=int(splitGHSmallTime[2])
+    )
+    return converted
+
+
+def downloadBuild(repository, sha):
+    """
+    Download the latest FloatingTools build from the passed sha.
+    :param repository: 
+    :param sha: 
+    :return: 
+    """
+    for fo in repository.get_dir_contents('/FloatingTools/'):
+        if fo.type == 'dir':
+            continue
+
+        # pull server data
+        serverPath = fo.path
+        try:
+            fileContent = repository.get_contents(serverPath, ref=sha)
+            fileData = base64.b64decode(fileContent.content)
+            fileOut = open(os.path.join(FloatingTools.INSTALL_DIRECTORY, serverPath), "w")
+            fileOut.write(fileData)
+            fileOut.close()
+        except (github.GithubException, IOError):
+            print 'Failed to update ' + serverPath
+
+
+def loadBranch():
+    """
+    Load the branch information from the Branch.json file.
+    :return: 
+    """
+    branchFile = os.path.join(FloatingTools.FLOATING_TOOLS_ROOT, 'Branch.json')
+    # create the file if it doesnt exists.
+    if not os.path.exists(branchFile):
+        # build the default release data
+        branchData = {'branch': 'master', 'build-date': None, 'build-time': None}
+        # dump the data
+        json.dump(branchData, open(branchFile, 'w'))
+    # load the branch data
+    branchData = json.load(open(branchFile, 'r'))
+
+    # connect to github and pull the FloatingTools repository.
+    hub = github.Github()
+    repository = hub.get_repo('aldmbmtl/FloatingTools')
+    commit = None
+
+    # find the branch being requested
+    for branch in repository.get_branches():
+        if branch.name == branchData['branch']:
+            commit = branch.commit
+            break
+
+    # grab the sha tag for loading from the branch.
+    sha = commit.sha
+    latestBuildData = githubDate2Datetime(commit.stats.last_modified)
+
+    # begin download
+    if branchData['build-date'] is None:
+        downloadBuild(repository, sha)
+
+        # update the branch data
+        branchData['build-date'] = latestBuildData.date().isoformat()
+        branchData['build-time'] = latestBuildData.time().isoformat()
+
+        # save out data
+        json.dump(branchData, open(branchFile, 'w'))
+
+    elif datetime.datetime(year=int(branchData['build-date'].split('-')[0]),
+                           month=int(branchData['build-date'].split('-')[1]),
+                           day=int(branchData['build-date'].split('-')[2]),
+                           hour=int(branchData['build-time'].split(':')[0]),
+                           minute=int(branchData['build-time'].split(':')[1]),
+                           second=int(branchData['build-time'].split(':')[2])) < latestBuildData:
+        downloadBuild(repository, sha)
+
+        # update the branch data
+        branchData['build-date'] = latestBuildData.date().isoformat()
+        branchData['build-time'] = latestBuildData.time().isoformat()
+
+        # save out data
+        json.dump(branchData, open(branchFile, 'w'))
+
+    else:
+        pass
+
+
+loadBranch()
