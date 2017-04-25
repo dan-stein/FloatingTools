f8789c4e0e4297ea5a3ea1a896da6b73ea3c0717
alexhatfield <alex.e.hatfield@gmail.com>
2017-04-25 00:55:22 -0400

    Adding setup.py for pip installing.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='FloatingTools',
     version='v0.1',
-    packages=['jwt', 'jwt.contrib', 'jwt.contrib.algorithms', 'click', 'flask', 'flask.ext', 'github', 'github.tests',
-              'jinja2', 'werkzeug', 'werkzeug.debug', 'werkzeug.contrib', 'markupsafe'],
+    packages=['FloatingTools'],
+    install_requires=['Flask', 'PyGithub'],
     url='https://github.com/aldmbmtl/FloatingTools',
     license='MIT',
     author='Alex Hatfield',
