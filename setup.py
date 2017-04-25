0ecf4ea532f616a6b86d96ca06debd83a6752723
alexhatfield <alex.e.hatfield@gmail.com>
2017-04-25 01:00:35 -0400

    Adding setup.py for pip installing.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(
     name='FloatingTools',
     version='v0.1',
-    packages=['FloatingTools'],
+    packages=find_packages(),
     install_requires=['Flask', 'PyGithub'],
     url='https://github.com/aldmbmtl/FloatingTools',
     license='MIT',
