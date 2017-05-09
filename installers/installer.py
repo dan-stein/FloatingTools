72df253a7cbb704de160c7a03edaad766fd56f75
alexhatfield <alex.e.hatfield@gmail.com>
2017-05-09 02:11:05 -0400

    Adding installers for windows, osx, and linux.

--- installers/installer.py
+++ installers/installer.py
@@ -2,8 +2,16 @@
 Installer file for FloatingTools
 """
 import sys
+import shutil
+import traceback
 from os.path import dirname
 
 sys.path.append(dirname(dirname(__file__)))
 
-import FloatingTools
+try:
+    import FloatingTools
+
+    print "Floating Tools installed and ready. Cleaning up..."
+    shutil.rmtree(dirname(__file__))
+except ImportError:
+    traceback.print_exc()
